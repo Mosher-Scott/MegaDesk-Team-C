a90ee3e5e83dc9ca9c3c991e3d66503d7d6d96d4
Rachel Bichsel <rachbichsel@gmail.com>
2020-01-29 22:38:04 -0700

    Add txt. Getting started on GetRushOrder()

--- MegaDesk-Mosher/DeskQuote.cs
+++ MegaDesk-Mosher/DeskQuote.cs
@@ -9,4 +9,32 @@ namespace MegaDesk_Mosher
     class DeskQuote
     {
     }
+
+    //Get Rush Order
+      public int GetRushOrder(){
+      string[] orderPrices = File.ReadAllLines("C:\\rushOrderPrices.txt");
+      int[,] rushOrderGrid = new int[3,3];
+        //outer loop for rows, inner for columns
+            for ( int i = 0; i < 3; i++) {
+                for ( int j = 0; j < 3; j++) {
+              rushOrderGrid[i][j] = orderPrices;
+           }
+           statement(s);
+        }
+            
+              try 
+	        {	        
+		
+	        }
+	        catch (Exception)
+	        {
+
+		        throw;
+	        }
+     
+      }
+      
+     
+
+    
 }
