85f5176389123149bb7c9b475e73e37d26833724
Mosher-Scott <46462152+Mosher-Scott@users.noreply.github.com>
2020-01-31 13:05:19 -0700

    Scott's (#2)
    * Did some edits
    
    * Added a comment
    
    * Did some stuff
    
    * Deleted some stuff
    
    * misc
    
    * Misc

--- MegaDesk-Mosher/ViewAlLQuotes.Designer.cs
+++ MegaDesk-Mosher/ViewAlLQuotes.Designer.cs
@@ -29,29 +29,43 @@
         private void InitializeComponent()
         {
             this.CloseFormButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // CloseFormButton
             // 
-            this.CloseFormButton.Location = new System.Drawing.Point(12, 400);
+            this.CloseFormButton.Location = new System.Drawing.Point(16, 457);
+            this.CloseFormButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.CloseFormButton.Name = "CloseFormButton";
-            this.CloseFormButton.Size = new System.Drawing.Size(111, 38);
+            this.CloseFormButton.Size = new System.Drawing.Size(148, 43);
             this.CloseFormButton.TabIndex = 2;
             this.CloseFormButton.Text = "&Return To Main";
             this.CloseFormButton.UseVisualStyleBackColor = true;
             this.CloseFormButton.Click += new System.EventHandler(this.CloseFormButton_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
             // ViewAllQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 514);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.CloseFormButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "ViewAllQuotes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "View All Quotes";
             this.Load += new System.EventHandler(this.ViewAllQuotes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +73,6 @@
         #endregion
 
         private System.Windows.Forms.Button CloseFormButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
